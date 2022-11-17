628fc4067bf9c253edfd2e190cf8b2918df90359
asharanjith <ashasvijayan@gmail.com>
2022-11-17 10:04:07 +0400

    wrap functions using async and await

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 import './style.css';
 import displayScores from './modules/api.js';
 
-// const submitButton = document.querySelector('.submitButton');
-
 const refreshButton = document.querySelector('#refresh');
 const form = document.querySelector('.inputPlayer');
 const message = document.querySelector('#message');
 const key = 'Fre06jK3qCMjBipi1fVQ';
+// const key = process.env.API_KEY;
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`;
 
 refreshButton.addEventListener('click', () => {
-  fetch(url,
-    {
-      method: 'GET',
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      displayScores(data.result);
-    });
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayScores(data.result);
+  };
+  fetchData();
 });
 
 form.addEventListener('submit', (e) => {
@@ -28,20 +25,29 @@ form.addEventListener('submit', (e) => {
     user: name,
     score,
   };
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => response.json());
-  message.innerHTML = 'Score submitted successfully';
-  message.classList.add('success');
-  setTimeout(() => {
-    message.innerHTML = '';
-    message.classList.remove('success');
-  }, 3000);
+
+  const postData = async () => {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const returndata = await response.json();
+    if (returndata.result === 'Leaderboard score created correctly.') {
+      message.innerHTML = 'Score submitted successfully';
+      message.classList.add('success');
+      setTimeout(() => {
+        message.innerHTML = '';
+        message.classList.remove('success');
+      },
+      3000);
+    }
+  };
+
   document.querySelector('#playerName').value = '';
   document.querySelector('#playerScore').value = '';
+
+  postData();
 });
